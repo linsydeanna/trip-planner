609ad1cda1e83b99bd9544d331dedb6bfcd9c4a7
Linsy <Linsy.Joyner@gmail.com>
2017-05-29 21:15:39 -0400

    data structure - first draft

---
+++ data.js
@@ -0,0 +1,51 @@
+const user = {
+  email: 'linsy.joyner@gmail.com',
+  username: 'linsydeanna',
+  password: 'supersecretpassword',
+  _id: '592c8029c1e6f9d97b6b146b',
+  trips: [
+    {
+      name: 'Europe 2017',
+      departureDate: '10/9/17',
+      returnDate: '10/16/17',
+      cities: [
+        {
+          city: 'Amsterdam',
+          country: 'Netherlands',
+          arrivalDate: '10/10/17',
+          departureDate: '10/13/17',
+          id: '1234'
+        }
+      ],
+      locations: [
+        {
+          locationName: 'Anne Frank House',
+          type: 'site',
+          address: 'Prinsengracht 263-267, 1016 GV, Amsterdam, NL',
+          date: '10/11/17',
+          startTime: '10:00am',
+          endTime: '12:00pm',
+          id: '1234'
+        },
+        {
+          locationName: 'Alchemist Garden',
+          type: 'restaurant',
+          address: 'Overtoom 409, 1054 JP, Amsterdam, NL',
+          date: '10/11/17',
+          startTime: '12:00pm',
+          endTime: '1:30pm',
+          id: '1234'
+        },
+        {
+          locationName: 'Harry Potter: The Exhibition!',
+          type: 'event',
+          address: 'CineMec Utrecht, Berlijnplein 100, 3541 CM UTRECHT',
+          date: '10/12/17',
+          startTime: '4:00pm',
+          endTime: '7:00pm',
+          id: '1234'
+        }
+      ]
+    }
+  ]
+};
