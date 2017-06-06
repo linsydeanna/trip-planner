e9d3ee70334d8cc4f2ecc18cb7fe3aab47e47c79
Vicky Leong <vleong2332@gmail.com>
2017-06-06 08:09:11 -0400

    Add validation to sign-up form

--- src/Validations.js
+++ src/Validations.js
@@ -5,3 +5,25 @@ export function required(value) {
     return 'Required';
   }
 }
+
+export function email(value) {
+  if (!isEmpty(value) && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return 'Invalid email address';
+  }
+}
+
+export function minLength(min) {
+  return value => {
+    if (!isEmpty(value) && value.length < min) {
+      return `Must be at least ${min} characters`;
+    }
+  };
+}
+
+export function maxLength(max) {
+  return value => {
+    if (!isEmpty(value) && value.length > max) {
+      return `Must be no more than ${max} characters`;
+    }
+  };
+}
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,21 +4,49 @@ import { Field, reduxForm } from 'redux-form';
 import Form from './ui/Form';
 import Button from './ui/Button';
 import Input from './ui/Input';
+import { required, email, minLength, maxLength } from '../Validations';
 
 import '../styles/components/_SignUpForm.scss';
 
-const SignUpForm = (props) => {
-  console.warn(props);
+const usernameLength = minLength(3) || maxLength(10);
+const passwordLength = minLength(4) || maxLength(10);
+
+const SignUpForm = props => {
+  const { handleSubmit } = props;
   return (
-    <Form className="sign-up-form" onSubmit={props.handleSubmit(props.onSubmit)}>
-      <label htmlFor="email">Email</label>
-      <Field id="email" name="email" component={Input} type="email" />
-      <label htmlFor="username">Username</label>
-      <Field id="username" name="username" component={Input} type="text" />
-      <label htmlFor="password">Password</label>
-      <Field id="password" name="password" component={Input} type="password" />
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <Field id="confirmPassword" name="confirmPassword" component={Input} type="password" />
+    <Form className="sign-up-form" onSubmit={handleSubmit}>
+      <Field
+        id="email"
+        name="email"
+        component={Input}
+        type="email"
+        label="Email"
+        validate={[required, email]}
+        />
+      <Field
+        id="username"
+        name="username"
+        component={Input}
+        type="text"
+        label="Username"
+        validate={[required, usernameLength]}
+      />
+      <Field
+        id="password"
+        name="password"
+        component={Input}
+        type="password"
+        label="Password"
+        validate={[required, passwordLength]}
+      />
+      <Field
+        id="confirm-password"
+        name="confirmPassword"
+        component={Input}
+        type="password"
+        label="Confirm Password"
+        validate={[required, passwordLength]}
+      />
       <Button btnStyle="primary" type="submit">Sign Up</Button>
     </Form>
   )
@@ -26,5 +54,5 @@ const SignUpForm = (props) => {
 
 export default reduxForm({
   form: 'signUp',
-  fields: ['email', 'username', 'password', 'confirmPassword']
+  destroyOnUnmount: false
 })(SignUpForm);
