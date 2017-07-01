4087975f8be26d982fe06dc7d8e0f58059320e7c
Vicky Leong <vleong2332@gmail.com>
2017-07-01 10:20:13 -0400

    Stub out navbar (#4)
    * Add logout, reset, and stubbed navbar
    
    * Restructure and clean up files
    
    * Style navbar a bit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdExpandMore } from 'react-icons/lib/md';
+import Card from './Card';
 
 import Logo from './Logo';
 
-const Navbar = (props) => {
+import '../styles/components/_Navbar.scss';
+
+const Navbar = props => {
+  let { user } = props;
   return (
     <div className="navbar">
       <Logo small />
       <nav className="nav">
-        <Link to="/logout">Logout</Link>
+        <Link to="/trips">Trips</Link>
       </nav>
+      <div className="user-menu">
+        { user.username }
+        <MdExpandMore className="icon-expand-more"/>
+        <div className="user-menu-context">
+          <Link to="/logout">Logout</Link>
+        </div>
+      </div>
     </div>
   );
 };
