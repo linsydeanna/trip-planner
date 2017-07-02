0964dd0410ee32ed58125b3b912b6ce81e9d08eb
Vicky Leong <vleong2332@gmail.com>
2017-07-01 22:41:25 -0400

    Style nav (#9)
    * Style nav
    
    * Make nav functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import Logo from './Logo';
 import UserMenu from './UserMenu';
 
 import '../styles/components/_Navbar.scss';
 
-class Navbar extends React.Component {
+const navList = [
+  { name: 'Trips', path: '/trips' }
+];
 
-  render() {
-    let { user } = this.props;
+const renderNavList = (navList, currentPath) => {
+  console.log('renderNavList', navList, currentPath);
+  return navList.map((nav, index) => {
+    let className = nav.path === currentPath ?
+      'link active' :
+      'link';
     return (
-      <div className="navbar">
-        <Logo small />
-        <nav className="nav">
-          <Link to="/trips">Trips</Link>
-        </nav>
-        <UserMenu user={user} />
-      </div>
+      <Link key={index} className={className} to={nav.path}>{nav.name}</Link>
     );
-  }
-
+  });
 }
 
-export default Navbar;
+const Navbar = ({ user, match }) => {
+  return (
+    <div className="navbar">
+      <Logo small />
+      <nav className="nav">
+        {renderNavList(navList, match.path)}
+      </nav>
+      <UserMenu user={user} />
+    </div>
+  );
+}
+
+export default withRouter(Navbar);
